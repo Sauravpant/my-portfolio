370a9f00d52f22fc02a5f3c3d6af4722d33eea67
Saurav Pant <sauravpant777@gmail.com>
2025-03-02 10:52:42 +0545

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -23,20 +23,6 @@ const AboutSection = () => {
           challenges and continuously improving my abilities. Let's connect!
         </p>
         <div className="mb-12"></div>
-        <div className="text-center mb-8">
-          <h2 className="text-4xl font-semibold mb-8">
-            Current Focus and Goals
-          </h2>
-          <p className="text-lg text-center max-w-3xl mx-auto">
-            I am currently diving into <strong>Competitive Programming </strong>
-            while focusing on the <strong>MERN Stack</strong> (MongoDB, Express,
-            React, Node.js) for backend development. Alongside this, I am
-            working on a <strong>Data Structure Visualizer Project</strong>{" "}
-            using MERN Stack to deepen my understanding of algorithms and improve
-            my practical coding skills. My goals include strengthening my
-            expertise in the MERN stack and competitive programming.
-          </p>
-        </div>
       </div>
       <div className="mb-12"></div>
     </div>
